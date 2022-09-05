3f4e39d595d9205e658a7d0b3c4f6e7c7f47a90c
Alejandro Borrego Megías <alejbormeg@gmail.com>
2022-09-05 10:15:02 +0200

    Update JsonHandler.cs

--- JsonProjectBE/JsonProjectBE/Handlers/JsonHandler.cs
+++ JsonProjectBE/JsonProjectBE/Handlers/JsonHandler.cs
@@ -16,7 +16,7 @@ namespace JsonProjectBE.Handlers
             document.originalFile = BsonDocument.Parse( request.Data.ToString() );
 
             JObject extracted= new JObject();
-            extracted.Add("prices", request.Data["prices"].ToString());
+            extracted.Add("prices", request.Data["prices"]?.ToString());
 
             document.extractedFile = BsonDocument.Parse( extracted.ToString() );
             return document;
